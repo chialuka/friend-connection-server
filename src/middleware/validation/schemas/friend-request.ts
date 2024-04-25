b91f6931bea796fc7906ea4c928936405186f659
Chukwualuka Chiama <alukachiama@gmail.com>
2024-04-26 00:15:32 +0100

    add endpoint for updating a friend request status

--- src/middleware/validation/schemas/friend-request.ts
+++ src/middleware/validation/schemas/friend-request.ts
@@ -4,3 +4,9 @@ export const FriendRequestSchema = z.object({
 	senderId: z.string({ required_error: 'senderId is required' }).uuid(),
 	receiverId: z.string({ required_error: 'receiverId is required' }).uuid(),
 });
+
+export const FriendRequestResponseSchema = z.object({
+	senderId: z.string({ required_error: 'senderId is required' }).uuid(),
+	receiverId: z.string({ required_error: 'receiverId is required' }).uuid(),
+	status: z.enum(['accepted', 'rejected']),
+});
--- src/routes/friend-request.ts
+++ src/routes/friend-request.ts
@@ -1,11 +1,17 @@
 import { Router } from 'express';
 
 import { validateRequestSchema } from '../middleware/validation';
-import { FriendRequestSchema } from '../middleware/validation/schemas/friend-request';
-import { createFriendRequest } from '../controllers/friend-request';
+import { FriendRequestResponseSchema, FriendRequestSchema } from '../middleware/validation/schemas/friend-request';
+import { createFriendRequest, updateFriendRequestStatus } from '../controllers/friend-request';
 
 const router = Router();
 
 router.post('/', validateRequestSchema({ schema: FriendRequestSchema, type: 'body' }), createFriendRequest);
+
+router.patch(
+	'/status',
+	validateRequestSchema({ schema: FriendRequestResponseSchema, type: 'body' }),
+	updateFriendRequestStatus,
+);
 
 export default router;
