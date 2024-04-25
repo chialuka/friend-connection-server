52d3c45ec157ce29aed44001721ea66740bc8996
Chukwualuka Chiama <alukachiama@gmail.com>
2024-04-25 11:35:15 +0100

    Add environmental variable files for prod, dev, and test environments
    To enable using different databases for production, development, and testing, this PR allows the app load the relevant env file depending on the value of NODE_ENV

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import express from 'express';
 import { config } from 'dotenv';
 
-config();
+const { NODE_ENV } = process.env;
+
+const envFile = NODE_ENV === 'production' ? '.env' : NODE_ENV === 'test' ? '.env.test' : '.env.development';
+
+config({ path: envFile });
 
 const PORT = process.env.PORT || 4000;
 
