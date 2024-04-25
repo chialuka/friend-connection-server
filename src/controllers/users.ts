e5b3be25b85cfb98addfbd278a5b0d1cb84c082c
Chukwualuka Chiama <alukachiama@gmail.com>
2024-04-26 00:22:34 +0100

    Get all users endpoint
    Add endpoint to get all users to be used for users who aren't logged in

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -18,3 +18,13 @@ export const createUser = async (req: Request, res: Response, next: NextFunction
 		next({ message: 'Error creating user', cause: error });
 	}
 };
+
+export const getAllUsers = async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		const users = await prisma.user.findMany();
+
+		return res.status(200).json({ users });
+	} catch (error) {
+		next({ message: 'Error getting all users', cause: error });
+	}
+};
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 
-import { createUser } from '../controllers/users';
+import { createUser, getAllUsers } from '../controllers/users';
 import { validateRequestSchema } from '../middleware/validation';
 import { CreateUserSchema } from '../middleware/validation/schemas/user';
 
 const router = Router();
 
 router.post('/', validateRequestSchema({ schema: CreateUserSchema, type: 'body' }), createUser);
+
+router.get('/', getAllUsers);
 
 export default router;
