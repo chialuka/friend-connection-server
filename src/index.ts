8dcab560db30a2cbf759ad9d82c46523436449a7
Chukwualuka Chiama <alukachiama@gmail.com>
2024-04-25 07:46:07 +0100

    Set project up with typescript and express
    Project setup with TypeScript, and express server and pnpm package runner

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import { config } from 'dotenv';
+
+config();
+
+const PORT = process.env.PORT || 4000;
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+
