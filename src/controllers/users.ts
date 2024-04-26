6a81cfd60f01d6f752df2341d79a4eeeace447a6
Chukwualuka Chiama <alukachiama@gmail.com>
2024-04-26 01:02:27 +0100

    Add endpoint to get non-friends of the requesting user
    This endpoint gets users, then filters and returns those who aren't friends of the requesting user

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -21,9 +21,43 @@ export const createUser = async (req: Request, res: Response, next: NextFunction
 
 export const getAllUsers = async (req: Request, res: Response, next: NextFunction) => {
 	try {
-		const users = await prisma.user.findMany();
+		const { userId } = req.params;
+		const users = await prisma.user.findMany({
+			where: {
+				userId: {
+					not: userId,
+				},
+			},
+		});
 
 		return res.status(200).json({ users });
+	} catch (error) {
+		next({ message: 'Error getting all users', cause: error });
+	}
+};
+
+export const getNonFriends = async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		const { userId } = req.params;
+		const users = await prisma.user.findMany({
+			where: {
+				userId: {
+					not: userId,
+				},
+			},
+		});
+		const friends = await prisma.connection.findMany({
+			where: {
+				OR: [{ userId }, { friendId: userId }],
+			},
+			select: {
+				userId: true,
+			},
+		});
+		const friendIds = friends.map((friend) => friend.userId);
+		const nonUserFriends = users.filter((user) => !friendIds.includes(user.userId));
+
+		return res.status(200).json({ nonUserFriends });
 	} catch (error) {
 		next({ message: 'Error getting all users', cause: error });
 	}
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 
-import { createUser, getAllUsers } from '../controllers/users';
+import { createUser, getAllUsers, getNonFriends } from '../controllers/users';
 import { validateRequestSchema } from '../middleware/validation';
 import { CreateUserSchema } from '../middleware/validation/schemas/user';
 
@@ -8,6 +8,8 @@ const router = Router();
 
 router.post('/', validateRequestSchema({ schema: CreateUserSchema, type: 'body' }), createUser);
 
-router.get('/', getAllUsers);
+router.get('/:userId', getAllUsers);
+
+router.get('/non-friends/:userId', getNonFriends);
 
 export default router;
